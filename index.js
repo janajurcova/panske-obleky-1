3e72dbce7c26463a68f2a189358fc080489d27f6
Iveta Žižková <ziz.iveta@gmail.com>
2022-04-19 20:24:20 +0200

    Přidání JS na button

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log('funguju');
+
+const changeColor = () => {
+  const svgElm = document.querySelector('#product-image');
+  svgElm.style.color = '#ffffff';
+};
+
+const btnElm = document.querySelector('.bila');
+btnElm.addEventListener('click', changeColor);
